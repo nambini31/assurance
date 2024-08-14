76e209848d45809ad4ff20e6f9ecf7523683a9f0
Nico <Nico@DESKTOP-95887V2>
2024-08-14 11:14:52 +0200

    nico

--- app/Views/header/header.php
+++ app/Views/header/header.php
@@ -1,13 +1,12 @@
-<nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-semi-dark" style="background-color: #1978ff !important;
+<nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-semi-dark" style="background-color: #05d363 !important;
     background-repeat: repeat-x;">
     <div class=" navbar-wrapper">
         <div class="navbar-header">
             <ul class="nav navbar-nav flex-row">
                 <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
-                <li class="nav-item"><a class="navbar-brand" href="/dashboard">
+                <li class="nav-item"><a class="navbar-brand" href="#">
 
-                        <h3 class="brand-text" style="margin-top: 10px; color : white">LOGO</h3>
-                        <!-- <h3 class="brand-text" style="margin-top: 10px;"><img class="brand-logo" style="width: 125px;" alt="modern admin logo" src="<?php echo base_url() ?>/assets/logo/logo.png"></h3> -->
+                        <h3 class="brand-text" style="margin-top: 10px;"><img class="brand-logo" style="width: 90px;" alt="modern admin logo" src="<?php echo base_url() ?>/assets/logo/logo.png"></h3>
                     </a></li>
                 <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"> <i class="la la-ellipsis-v"></i></a></li>
             </ul>
