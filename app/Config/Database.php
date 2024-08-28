7d019b12771f4178dcc739e049af01d4c6e2f064
jpfaobe1234 <jp1234user@gmail.com>
2024-08-28 14:14:41 +0200

    bibiby

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => 'nico1234',
+        'password'     => '',
         'database'     => 'assurance',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
